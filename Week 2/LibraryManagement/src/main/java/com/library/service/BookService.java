d3699f3d36f5555dec84a1cd066aea884fe21edc
Niranjan <nirur004@gmail.com>
2024-08-07 18:11:48 +0530

    week 2

---
+++ Week 2/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,21 @@
+package com.library.service;
+
+import org.springframework.stereotype.Service;
+import com.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Service
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    @Autowired
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void displayBooks() {
+        bookRepository.getBooks().forEach(System.out::println);
+    }
+}
+
